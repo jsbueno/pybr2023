59a12ff87b2f867b95b4a246da68b271aa4c7b58
veronica <veronica.souza.silva.97@gmail.com>
2023-11-05 13:05:36 -0500

    Game exception base and defeat classes

--- jogomapa/game_exceptions.py
+++ jogomapa/game_exceptions.py
@@ -1,4 +1,15 @@
-class GameWinException(BaseException):
+class GameException(BaseException):
+    """
+    Base game exception.
+    """
+
+
+class GameWinException(GameException):
     """
     Raises when the game finishes by win condition.
+    """
+
+class GameDefeatException(GameException):
+    """
+    Raises when the game finishes by defeat condition
     """
